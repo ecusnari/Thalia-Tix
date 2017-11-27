9d723a14d4e026a40a4af0ff8124be057a48eb8f
ERICA\Ericas <ecusnari@hawk.iit.edu>
2017-11-27 08:12:08 +0200

    fixing build.xml

--- ThaliaTixProject/src/Testing/BasicTests.java
+++ ThaliaTixProject/src/Testing/BasicTests.java
@@ -5,9 +5,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
